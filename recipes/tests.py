c0b14adf52f01f9069e78be9bb1703e9b344f832
Thien <devnull@localhost>
2010-01-19 22:41:09 -0500

    Wrote a test for recipe_list view

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,23 +1,8 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+class RecipeListTest(TestCase):
+	#to make sure the recipe list page will load without errors
+	def test_recipe_list(self):
+		response = self.client.get("/cookbook/recipes/")
+		self.assertEqual(response.status_code, 200)
+		
